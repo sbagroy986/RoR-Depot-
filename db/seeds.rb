e6bbd107df82af172ab21c583e7e4321a643179f
sbagroy986 <shrey14099@iiitd.ac.in>
2014-09-27 12:15:24 +0530

    Depot v1

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Product.delete_all
+Product.create!(title: 'Chyawanprash',
+	description:
+		%{<p>
+			Do you even lift bro?
+			},
+	image_url: 'http://www.herbal-market.com/images/Dabur%20Chyawanprash.jpg',
+	price: 420.0
+	)
